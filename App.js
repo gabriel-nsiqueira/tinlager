d02e6b4965656c3a1da09b314d43d6ab121a0d19
Gabriel <gnevessiqueira@gmail.com>
2019-11-23 17:02:12 -0300

    fix

--- App.js
+++ App.js
@@ -1,6 +1,12 @@
 import React from 'react';
 
-import { View, StatusBar, Image, TouchableOpacity, StyleSheet } from 'react-native';
+import {
+  View,
+  StatusBar,
+  Image,
+  TouchableOpacity,
+  StyleSheet,
+} from 'react-native';
 import jhow from './src/images/profile.png';
 import emerald from './src/images/emerald.png';
 
@@ -13,57 +19,61 @@ import SignUp from './src/pages/signUp/index';
 import SignIn from './src/pages/signIn/index';
 
 const styles = StyleSheet.create({
-	header: {
-		flexDirection: 'row',
-		justifyContent: 'space-between',
-		padding: 15,
-		shadowColor: 'gray',
-		shadowOffset: { width: 1, height: 1 },
-		shadowOpacity: 0.18,
-		shadowRadius: 10,
-		backgroundColor: 'white'
-	}
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 15,
+    shadowColor: 'gray',
+    shadowOffset: { width: 1, height: 1 },
+    shadowOpacity: 0.18,
+    shadowRadius: 10,
+    backgroundColor: 'white',
+  },
 });
 
 export default createAppContainer(
-	createStackNavigator(
-		{
-			SignIn,
-			App,
-			Buy,
-			SignUp
-		},
-		{
-			defaultNavigationOptions: {
-				header: (props) => (
-					<View>
-						<StatusBar hidden={true} />
-						<View style={{ height: 60 }}>
-							<View style={styles.header}>
-								<TouchableOpacity onPress={{}}>
-									<Image
-										source={jhow}
-										style={{
-											width: 32,
-											height: 40,
-											marginLeft: 10
-										}}
-									/>
-								</TouchableOpacity>
-								<TouchableOpacity onPress={() => props.navigation.navigate('Buy')}>
-									<Image
-										source={emerald}
-										style={{
-											width: 32,
-											height: 40
-										}}
-									/>
-								</TouchableOpacity>
-							</View>
-						</View>
-					</View>
-				)
-			}
-		}
-	)
+  createStackNavigator(
+    {
+      SignIn,
+      App,
+      Buy,
+      SignUp,
+    },
+    {
+      defaultNavigationOptions: {
+        header: props => (
+          <View>
+            <StatusBar hidden={true} />
+            <View style={{ height: 60 }}>
+              <View style={styles.header}>
+                <TouchableOpacity
+                  onPress={() => props.navigation.navigate('signIn')}
+                >
+                  <Image
+                    source={jhow}
+                    style={{
+                      width: 32,
+                      height: 40,
+                      marginLeft: 10,
+                    }}
+                  />
+                </TouchableOpacity>
+                <TouchableOpacity
+                  onPress={() => props.navigation.navigate('Buy')}
+                >
+                  <Image
+                    source={emerald}
+                    style={{
+                      width: 32,
+                      height: 40,
+                    }}
+                  />
+                </TouchableOpacity>
+              </View>
+            </View>
+          </View>
+        ),
+      },
+    },
+  ),
 );
